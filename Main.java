209d3af3e80221da6745c50be02e6713b515ddff
Izat1966 <150505243+Izat1966@users.noreply.github.com>
2024-10-25 16:58:50 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,5 +4,11 @@ public class Main {
         System.out.println(MathUtill.gcd(9, 12));
         System.out.println(MathUtill.lcm(9, 12));
         System.out.println(MathUtill.fibonacci(6));
+        System.out.println(MathUtill.factorial(6));
+        var mathUtill = new MathUtill();
+        System.out.println(mathUtill.isPerfect(6));
+        System.out.println(mathUtill.sumOfDigits(1269));
+        System.out.println(mathUtill.reverseNumber(1269));
+        System.out.println(mathUtill.nextPrime(17));
     }
 }
