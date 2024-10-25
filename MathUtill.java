87e1fd097d9f0c9755b19474203b92f3014e3b90
Izat1966 <150505243+Izat1966@users.noreply.github.com>
2024-10-25 16:58:26 +0600

    Update MathUtill.java

--- MathUtill.java
+++ MathUtill.java
@@ -1,15 +1,15 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class MathUtill {
- // 5 static methods
-        public static boolean isPrime(int a) {
-            for (int i = 2; i < a; i++) {
-                if (a % i == 0) {
-                    return false;
-                }
+    // 5 static methods
+    public static boolean isPrime(int a) {
+        for (int i = 2; i < a; i++) {
+            if (a % i == 0) {
+                return false;
             }
-            return true;
         }
+        return true;
+    }
     public static int gcd(int a, int b) {
         while (b != 0) {
             int ca = b;
@@ -32,5 +32,53 @@ public class MathUtill {
             b = va;
         }
         return b;
+    }
+    public static int factorial(int n) {
+        int me = 1;
+        for (int i = 1; i <= n; i++) {
+            me *= i;
+        }
+        return me;
+    }
+    // 4 non-static methods
+    public boolean isPerfect(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+        if (sum == n) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public int sumOfDigits(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+    public int reverseNumber(int n) {
+        int te = 0;
+        while (n > 0) {
+            int qe = n % 10;
+            te = te * 10 + qe;
+            n /= 10;
+        }
+        return te;
+    }
+    public int nextPrime(int n) {
+        int ca = n + 1;
+        while (!isPrime(ca)) {
+            ca++;
+        }
+        return ca;
     }
 }
